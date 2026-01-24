2f79ae118f9440852177884338bfecbaef065d68
ShamsParvezArka <parvez6826@gmail.com>
2026-01-24 13:29:05 +0600

    fix -> rigid code

--- script/player_basic/pick_up.gd
+++ script/player_basic/pick_up.gd
@@ -1,22 +1,22 @@
 extends State
 
-var can_pickup := false
 var object: StaticBody2D
 
 func enter() -> void:
-	if !can_pickup:
+	if object == null:
 		state_machine.change_state(state_machine.previous_state)
+		return
 	
-
-func update(delta: float) -> void:
-	if can_pickup:
-		object.queue_free()
-		can_pickup = false
-		state_machine.change_state(state_machine.previous_state)
-	
+	object.queue_free()
+	object = null
+	state_machine.change_state(state_machine.previous_state)
+		
 
 func _on_interaction_area_body_entered(body: Node2D) -> void:
 	if body.is_in_group("collectable"):
-		print("collectable")
-		can_pickup = true
 		object = body
+		
+
+func _on_hit_area_body_exited(body: Node2D) -> void:
+	if body == object:
+		object = null
