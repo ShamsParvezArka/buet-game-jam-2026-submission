aa9160e979b76b8f50c0066b15cd5945788000ae
ShamsParvezArka <parvez6826@gmail.com>
2026-01-24 23:01:04 +0600

    new -> tutorial level

--- script/global_state.gd
+++ script/global_state.gd
@@ -11,5 +11,5 @@ var player_basic_apply_gravity: bool
 var player_basic_mid_air: bool
 var can_climb: bool
 
-var anime_sword_obtained := true
+var anime_sword_obtained := false
 var is_holding := false
